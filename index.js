e1be8a10f351dfdaef880411c23da83072dd4a9c
Bethany Mai <bethany.mai@shopko.com>
2018-07-30 17:43:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
    return ("I can\'t hear you!");
 }
-}
-function sayHiToGrandma(string){
+
  if (string.toUpperCase()) {
     return ("YES INDEED!");
 }
-}
-function sayHiToGrandma(string){
+
   if  (string == "I love you, Grandma.") {
     return ("I love you, too.");
 }
