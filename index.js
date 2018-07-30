066783cc4d19e87a4636115c95a8ca40adf7f27b
Bethany Mai <bethany.mai@shopko.com>
2018-07-30 17:40:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
    return ("I can\'t hear you!");
 }
   else if
-   (string.toUpperCase()) {
+   ('hello'.toUpperCase()) {
     return ("YES INDEED!");
 }
   else if
